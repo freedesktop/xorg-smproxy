83e5545ee56d73a0b1b37683397b303371c9256a
Alan Coopersmith <alan.coopersmith@oracle.com>
2014-11-05 17:30:50 -0800

    Remove unneeded checks for X_NOT_POSIX
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- smproxy.h
+++ smproxy.h
@@ -47,7 +47,6 @@ Author:  Ralph Mor, X Consortium
 #endif
 #include <stdlib.h>
 
-#ifndef X_NOT_POSIX
 #ifdef _POSIX_SOURCE
 #include <limits.h>
 #else
@@ -55,7 +54,7 @@ Author:  Ralph Mor, X Consortium
 #include <limits.h>
 #undef _POSIX_SOURCE
 #endif
-#endif /* X_NOT_POSIX */
+
 #ifndef PATH_MAX
 #include <sys/param.h>
 #ifndef PATH_MAX
